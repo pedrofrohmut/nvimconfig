b017d94461d0b43d9a9942ba9a1346626cb4bb11
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-09-01 11:41:21 -0300

    Lualine shows FileFormat

--- lua/didorgas/plugins/lualine.lua
+++ lua/didorgas/plugins/lualine.lua
@@ -1,35 +1,37 @@
-require('lualine').setup {
+require("lualine").setup {
     options = {
-        component_separators = { left = '|', right = '|'},
+        component_separators = { left = "|", right = "|"},
     },
     sections = {
-        lualine_a = {},
+        -- Left side a, b, c
+        lualine_a = {"mode"},
         lualine_b = {
             {
-                'filename',
+                "filename",
                 file_status = true,
                 newfile_status = false,
                 path = 1,
                 shorting_target = 40,
                 symbols = {
-                    modified = '[+]',
-                    readonly = '[-]',
-                    unnamed = '[No Name]',
-                    newfile = '[New]',
+                    modified = "[+]",
+                    readonly = "[-]",
+                    unnamed = "[No Name]",
+                    newfile = "[New]",
                 }
             }
         },
         lualine_c = {},
-        lualine_x = {'filetype', 'progress', 'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
+
+        -- Right Side x, y, z
+        lualine_x = {
+            "filetype",
+            {
+                "fileformat",
+                symbols = { unix = "UNIX", dos = "DOS", mac = "MAC" }
+            },
+            "encoding"
+        },
+        lualine_y = {"progress"},
+        lualine_z = {"location"}
+    }
 }
