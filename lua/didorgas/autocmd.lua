b2f2e3cb85d31ce211759f527acf5def656d2b97
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-09-07 14:03:41 -0300

    HighLight matching paren & Cursorline only on focused window

--- lua/didorgas/autocmd.lua
+++ lua/didorgas/autocmd.lua
@@ -1,5 +1,16 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
 -- Remove auto insert comments next line
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = {"*"},
     command = "setlocal formatoptions-=cro"
 })
+
+-- Only show the cursor line in the active buffer.
+augroup("CursorLine", { clear = true })
+autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+    group = "CursorLine", pattern = { "*" }, command = "setlocal cursorline" })
+autocmd({ "WinLeave" }, {
+    group = "CursorLine", pattern = { "*" }, command = "setlocal nocursorline" })
+
--- lua/didorgas/colors.lua
+++ lua/didorgas/colors.lua
@@ -3,11 +3,14 @@ vim.opt.background = "dark"
 -- The configuration options should be placed before `colorscheme sonokai`.
 vim.cmd [[ let g:sonokai_style = 'shusia' ]]
 vim.cmd [[ let g:sonokai_better_performance = 1 ]]
+vim.cmd [[ let g:sonokai_transparent_background = 2 ]]
 
 vim.cmd [[ colorscheme sonokai ]]
 
+vim.cmd [[ highlight MatchParen gui=bold guifg=#ff3333 guibg=None ]]
+
 -- Transparency
-vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
-vim.cmd [[ hi LineNr guibg=NONE ctermbg=NONE ]]
-vim.cmd [[ hi SignColumn guibg=NONE ctermbg=NONE ]]
-vim.cmd [[ hi EndOfBuffer guibg=NONE ctermbg=NONE ]]
+-- vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
+-- vim.cmd [[ hi LineNr guibg=NONE ctermbg=NONE ]]
+-- vim.cmd [[ hi SignColumn guibg=NONE ctermbg=NONE ]]
+-- vim.cmd [[ hi EndOfBuffer guibg=NONE ctermbg=NONE ]]
