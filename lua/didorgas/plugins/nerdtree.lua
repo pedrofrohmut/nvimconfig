f29c2e217f6cf9deb2de463a977c9a675df75d21
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-09-07 22:03:01 -0300

    NERDTree conf updt to fix bug

--- lua/didorgas/plugins/nerdtree.lua
+++ lua/didorgas/plugins/nerdtree.lua
@@ -7,5 +7,13 @@ vim.cmd [[
 
 vim.cmd [[ let NERDTreeWinSize = 40 ]]
 
+vim.cmd [[ let NERDTreeHijackNetrw = 1 ]]
+
+vim.cmd [[ let NERDTreeShowHidden = 1 ]]
+
+vim.cmd [[ let NERDTreeMinimalUI = 1 ]]
+
+vim.cmd [[ let NERDTreeMinimalMenu = 1 ]]
+
 vim.keymap.set("n", "<leader>ft", "<cmd>NERDTreeFind<Enter>")
 vim.keymap.set("n", "<C-b>", "<cmd>NERDTreeToggle<Enter>")
